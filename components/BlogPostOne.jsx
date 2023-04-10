e91c9f0b79c108dc92054cdf072d8bc2434c8416
Patrick Orihuela <patrick.orihuela@gmail.com>
2023-04-09 18:42:58 -0600

    Tab Selector with basic UI implementation

---
+++ components/BlogPostOne.jsx
@@ -0,0 +1,201 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/BlogPostOne.module.css";
+
+const BlogPostOne = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.text}>
+        <div className={styles.titleGroup}>
+          <h1 className={styles.title}>Blog Post One by Patrick Orihuela</h1>
+        </div>
+        <div className={styles.textGroup}>
+          <h3>A Strong Tool for Web Development Is TypeScript</h3>
+          <p>
+            The robustness and maintainability of code are enhanced with the
+            addition of optional static typing and other capabilities in
+            TypeScript, a superset of JavaScript. TypeScript has grown
+            significantly in popularity since its launch in 2012 and is
+            currently commonly utilized in the sector. This article will cover
+            the effects of TypeScript on development teams and workflows,
+            frequent errors and traps to watch out for while using TypeScript,
+            and advice for switching from JavaScript to TypeScript. We&apos;ll
+            also provide examples of actual businesses and projects that have
+            used TypeScript, along with their feedback.
+          </p>
+        </div>
+
+        <div className={styles.textGroup}>
+          <h3>TypeScript&apos;s Effects on Workflows and Development Teams</h3>
+          <p>
+            The industry&apos;s development teams and workflows have been
+            greatly impacted by TypeScript. Since it can offer a more stable and
+            manageable codebase, it has grown in popularity. There have been
+            fewer mistakes and a smoother development process as a result of the
+            implementation of static typing, which has aided in catching flaws
+            early in the development process. Development teams have benefited
+            from this since they can now produce high-quality code more quickly.
+            Several development teams like TypeScript because it can be
+            integrated with well-known JavaScript libraries and frameworks. In
+            general, TypeScript has streamlined development procedures,
+            resulting in teams that are more effective and productive.
+          </p>
+        </div>
+        <div className={styles.textGroup}>
+          <h3> Common Errors and TypeScript Pitfalls to Avoid</h3>
+          <p>
+            While using TypeScript, there are frequent errors and hazards to
+            watch out for just as with any other programming language. While
+            TypeScript is essentially a superset of JavaScript, people
+            frequently make the mistake of thinking it is a substitute for
+            JavaScript. JavaScript is built upon by TypeScript, which also adds
+            new capabilities and functionality. Another error is to use
+            interfaces or type annotations excessively or insufficiently.
+            Underusing features might result in less maintainable code while
+            overusing them can result in bloated code. It&apos;s crucial to find
+            a balance and make advantage of TypeScript features when necessary.
+            Additional traps to watch out for include using the TypeScript
+            compiler incorrectly, not adhering to code standards, and not
+            staying current with new TypeScript versions.
+          </p>
+        </div>
+        <div className={styles.textGroup}>
+          <h3>
+            Tips and Best Practices for Transitioning from JavaScript to
+            TypeScript
+          </h3>
+          <p>
+            Transitioning from JavaScript to TypeScript can be a daunting task,
+            but there are some tips and best practices that can make the process
+            smoother. One important tip is to start small and gradually
+            transition to TypeScript. Begin by adding TypeScript to a few files
+            or modules and gradually expand from there. This approach can help
+            ease developers into the new language and prevent overwhelm.
+            Additionally, it is important to understand the key differences
+            between JavaScript and TypeScript, such as the addition of static
+            typing and interfaces. These new features should be studied by
+            developers to see how they might improve their codebase. Lastly, it
+            is advised to utilize TypeScript with a contemporary code editor or
+            integrated programming environment (IDE), such as Microsoft Studio
+            Code, which offers robust TypeScript support and can speed up the
+            development process.
+          </p>
+        </div>
+        <div className={styles.textGroup}>
+          <h3>
+            Real-World Examples of Companies and Projects that have Adopted
+            TypeScript
+          </h3>
+          <p>
+            There are many examples of companies and projects that have
+            successfully adopted TypeScript and seen positive results. One such
+            example is Slack, which adopted TypeScript to improve code quality
+            and maintainability. According to Slack&apos;s engineering team,
+            TypeScript has helped catch errors earlier in the development
+            process and reduced the overall number of bugs in the codebase.
+            Another example is Asana, which adopted TypeScript to help with code
+            maintainability and scalability. Asana&apos;s engineering team found
+            that TypeScript allowed them to easily refactor code and ensure that
+            changes were propagated correctly throughout the codebase. Other
+            organizations and initiatives that have used TypeScript include
+            Angular, Microsoft, and Airbnb. These examples highlight the
+            advantages of TypeScript in practical situations and show how it may
+            enhance team productivity and development workflows.
+          </p>
+          <ol>
+            <li>
+              <b>The Impact of TypeScript on Development Teams and Workflows</b>
+              <p>
+                Example: To enhance the quality and maintainability of their
+                codebase, the Slack development team switched to TypeScript.
+                They were able to find mistakes sooner in the development
+                process and lower the amount of problems in their codebase by
+                utilizing TypeScript&apos;s static typing. This made the
+                development process go more smoothly and produced code of
+                greater quality.
+              </p>
+            </li>
+            <li>
+              <b>
+                Typical Mistakes and TypeScript Pitfalls to Avoid Illustration:
+              </b>
+              <p>
+                Overusing interfaces or type annotations in TypeScript is a
+                common mistake that can lead to bloated code. Using type
+                annotations for every variable, even when they are not required,
+                is an illustration of this. Developers should refrain from doing
+                this by only using type annotations when they are required to
+                increase the readability or maintainability of their code.
+              </p>
+            </li>
+            <li>
+              <b>
+                Transitioning from JavaScript to TypeScript: Recommended
+                Practices and Advice
+              </b>
+              <p>
+                Example: It&apos;s crucial to start small and switch over to
+                TypeScript gradually while switching from JavaScript. An
+                illustration of this would be to add TypeScript to a small
+                number of files or modules first and then progressively extend
+                from there. This can lessen the learning curve for developers
+                and reduce confusion. Understanding the main distinctions
+                between JavaScript and TypeScript, such as the inclusion of
+                static typing and interfaces, is also crucial. Developers may
+                more effectively use TypeScript&apos;s features and enhance
+                their codebase by being aware of these differences.
+              </p>
+            </li>
+            <li>
+              <b>
+                Outlining how certain well-known frameworks or libraries, such
+                React or Angular, incorporate TypeScript
+              </b>
+              <p>
+                Example: With Angular, a well-liked web application framework,
+                TypeScript is frequently employed. Type annotations are provided
+                for components, services, and other modules in Angular using
+                TypeScript. This makes code easier to understand, maintain, and
+                find faults sooner in the development process. Moreover,
+                TypeScript is supported natively by Angular, making it simple to
+                include into an Angular project.
+              </p>
+            </li>
+          </ol>
+        </div>
+
+        <div className={styles.textGroup}>
+          <p>
+            To sum up, TypeScript is a potent and well-liked programming
+            language that has a lot to offer development teams and processes.
+            Many development teams now use TypeScript because of its capacity to
+            produce more robust and manageable codebases, identify mistakes
+            early in the development process, and interact with well-liked
+            JavaScript libraries and frameworks.
+          </p>
+          <p>
+            While using TypeScript, there are several frequent errors and
+            hazards to watch out for, just as with any other programming
+            language. Striking a balance between adhering to code standards,
+            using TypeScript capabilities when appropriate, and staying current
+            with new TypeScript releases is crucial. Making the switch from
+            JavaScript to TypeScript may also be challenging, but with the
+            correct strategy and resources, developers can speed up and
+            streamline the procedure.
+          </p>
+          <p>
+            Overall, TypeScript has made a big difference in the industry,
+            streamlining development processes and creating teams that are more
+            effective and productive. It is probable that TypeScript will
+            continue to be a favoured option for many development teams in the
+            years to come due to its continuing growth and development. Learning
+            and comprehending TypeScript as a developer may be a worthwhile
+            investment in your abilities and professional development.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogPostOne;
---
+++ components/BlogPostTwo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BlogPostTwo = () => {
+  return (
+    <div>BlogPostTwo</div>
+  )
+}
+
+export default BlogPostTwo
